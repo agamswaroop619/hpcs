af834bf8c6c5e1b86089287a719e01299b1d3738
Atam Swaroop <112094449+agamswaroop619@users.noreply.github.com>
2023-04-09 12:02:07 +0530

    Add files via upload

---
+++ Web/defaultvalues.js
@@ -0,0 +1,15 @@
+function changeTemp() {
+    var element = document.getElementById("my-text1");
+    element.innerHTML = "Current value";
+    
+  }
+  function changeNutri() {
+    var element = document.getElementById("my-text3");
+    element.innerHTML = "Current value";
+    
+  }
+  function changeMoisture() {
+    var element = document.getElementById("my-text2");
+    element.innerHTML = "Current value";
+ 
+  }
